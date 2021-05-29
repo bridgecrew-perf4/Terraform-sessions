514bad872fcb642e1f9fdd10ac177d7e53058efd
solongocyber <solongocyber@gmail.com>
2021-05-29 03:43:58 +0000

    lab-2

---
+++ Udemy/lab-2/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+ami = "ami-0d5eff06f840b45e9"
+instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.web.id]
+// user_data = file("")
+tags = {
+    Name = "web"
+    Project = "udemy"
+    Owner = "Solongo" 
+ }
+}
